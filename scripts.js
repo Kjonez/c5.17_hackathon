3c4034577c7058848e70bc9877bdd74079c47888
jonmorales89 <jon.sal.morales@gmail.com>
2017-06-20 20:32:20 -0700

    added in directional moves for player 1

--- scripts.js
+++ scripts.js
@@ -15,9 +15,13 @@ for(var i=0; i < 8; i++){
 }
 /* Initialize Starting Game Pieces*/
 $("[row='3'][col='3']").addClass('whitePlayer').removeClass('blankSpot');
+gameBoard[3][3]= "W";
 $("[row='3'][col='4']").addClass('blackPlayer').removeClass('blankSpot');
+gameBoard[3][4]='B';
 $("[row='4'][col='3']").addClass('blackPlayer').removeClass('blankSpot');
+gameBoard[4][3]='B';
 $("[row='4'][col='4']").addClass('whitePlayer').removeClass('blankSpot');
+gameBoard[4][4]='W';
 /*click handler*/
 $('td').click(setPiece);
 /* function / variable declaration */
@@ -29,11 +33,12 @@ var whitePieceCounter = 0;
 function setPiece(){
     if($(this).attr('class') === 'blankSpot'){
     if(!whoseTurn) {
-        $(this).addClass('blackPlayer').removeClass('blankSpot');
-        counter--;
         var testX = $(this).attr('row');
         var testY = $(this).attr('col');
+        checkLegalMove(testX, testY);
         gameBoard[testX][testY] = "B";
+        $(this).addClass('blackPlayer').removeClass('blankSpot');
+        counter--;
         whoseTurn = true;
     } else {
         $(this).addClass('whitePlayer').removeClass('blankSpot');
@@ -41,12 +46,56 @@ function setPiece(){
         counter--;
         var testX = $(this).attr('row');
         var testY = $(this).attr('col');
-        gameBoard[testX][testY] = "W"
+        gameBoard[testX][testY] = "W";
     }
         console.log(counter);
         if (counter === 0){
             countGameScore();
             endOfGame();
+        }
+    }
+}
+/* Check legal move */
+function checkLegalMove(x, y) {
+    var x = parseInt(x);
+    var y = parseInt(y);
+    //This is going in the right direction from the click point
+    if (gameBoard[x][y + 1] === "W" && gameBoard[x].includes("B", [y + 2])) {
+        for (var i = 1; i < gameBoard[x].length; i++) {
+            if (gameBoard[x][y + i] === "W") {
+                gameBoard[x][y + i] = "B";
+                $("[row='" + x + "'][col='" + (y + i) + "']").removeClass('whitePlayer').addClass("blackPlayer");
+            }
+        }
+        //Checking diagonally right direction;
+    } else if (gameBoard[x + 1][y + 1] === "W") {
+        var diagArray = [];
+        for (var i = 1; i < gameBoard.length - x; i++) {
+            diagArray.push(gameBoard[x + i][y + i]);
+            console.log(diagArray);
+            if (diagArray.includes("B")) {
+                for (var i = 1; i < diagArray.length; i++) {
+                    if (gameBoard[x + i][y + i] === "W") {
+                        gameBoard[x + i][y + i] = "B";
+                        $("[row='" + (x + i) + "'][col='" + (y + i) + "']").removeClass('whitePlayer').addClass("blackPlayer");
+                    }
+                }
+            }
+        }
+        //Checking vertically down direction;
+    } else if (gameBoard[x + 1][y] === "W") {
+        var vertArray = [];
+        for (var i = 1; i < gameBoard.length - x; i++) {
+            vertArray.push(gameBoard[x + i][y]);
+            console.log(vertArray);
+            if (vertArray.includes("B")) {
+                for (var i = 1; i < vertArray.length; i++) {
+                    if (gameBoard[x + i][y] === "W") {
+                        gameBoard[x + i][y] = "B";
+                        $("[row='" + (x + i) + "'][col='" + y + "']").removeClass('whitePlayer').addClass("blackPlayer");
+                    }
+                }
+            }
         }
     }
 }
